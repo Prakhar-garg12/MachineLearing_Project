c4a3c586eac31e8efb275c094f9e494bd2160984
Prakhar Garg <satishagrawal6993@gmail.com>
2026-05-20 15:03:45 +0530

    Data transformation

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -8,6 +8,9 @@ from dataclasses import dataclass
 from dotenv import load_dotenv
 import pymysql
 
+import pickle
+import numpy as np
+
 
 load_dotenv()
 
@@ -32,6 +35,19 @@ def read_sql_data():
         print(df.head())
 
         return df
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
